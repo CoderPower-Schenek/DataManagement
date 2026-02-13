f77c72df2be6d3cc6b07cfc62fac46d7304fc18c
CoderPower-Schenek <schenek_code_power@163.com>
2026-02-13 20:36:52 +0800

    Update __init__.py

--- DataManagement/__init__.py
+++ DataManagement/__init__.py
@@ -5,7 +5,7 @@ This package provides DataPoint and DataSerials classes for managing
 collections of data with powerful filtering, aggregation, and serialization.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Shihong Chen'
 __email__ = 'schenek@connect.ust.hk'
 
